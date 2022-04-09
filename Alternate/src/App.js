56b19446ad3f9f7edc11641f0a7161c922ceb423
Ted Green <shwj25@durham.ac.uk>
2022-04-09 22:54:59 +0100

    Adding Page Routing

--- Alternate/src/App.js
+++ Alternate/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
-
+import Addphone from "./AddPhone.js";
 const element =( 
-    <><h2>Phone Directory</h2>
+    <><div id='title_div'>
+    <h1 id="title">Phone Directory</h1></div>
     <div id='nav_div'>
       <button className="button">Home</button>
-      <button className ="button" >Add Phone Number</button>
+      <button type ="button" className ="button" onclick="alert('Hello world!')" >Add Phone Number</button>
+      <button type="button" onclick="alert('Hello world!')">Click Me!</button>
       <button className ="button">Search</button>
       <button className ="button">Favourites</button>
     </div></>)
@@ -17,3 +19,6 @@ ReactDOM.render(
   );
 }  
 export default App;
+
+
+
