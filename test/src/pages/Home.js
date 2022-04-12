4e4a120d6da6440d234661b0e4457b2bcb219928
Ted Green <shwj25@durham.ac.uk>
2022-04-12 02:19:21 +0100

    Changing appearence of the home page nav bars

--- test/src/pages/Home.js
+++ test/src/pages/Home.js
@@ -1,118 +1,65 @@
 import "./Home.css"
 const element =(<>
-    <a href="#a"className="alphabet"><button>a</button></a>
-    <a href="#b"className="alphabet"><button>b</button></a>
-    <a href="#c"className="alphabet"><button>c</button></a>
-    <a href="#d"className="alphabet"><button>d</button></a>
-    <a href="#e"className="alphabet"><button>e</button></a>
-    <a href="#f"className="alphabet"><button>f</button></a>
-    <a href="#g"className="alphabet"><button>g</button></a>
-    <a href="#h"className="alphabet"><button>h</button></a>
-    <a href="#i"className="alphabet"><button>i</button></a>
-    <a href="#j"className="alphabet"><button>j</button></a>
-    <a href="#k"className="alphabet"><button>k</button></a>
-    <a href="#l"className="alphabet"><button>l</button></a>
-    <a href="#m"className="alphabet"><button>m</button></a>
-    <a href="#n"className="alphabet"><button>n</button></a>
-    <a href="#o"className="alphabet"><button>o</button></a>
-    <a href="#p"className="alphabet"><button>p</button></a>
-    <a href="#q"className="alphabet"><button>q</button></a>
-    <a href="#r"className="alphabet"><button>r</button></a>
-    <a href="#s"className="alphabet"><button>s</button></a>
-    <a href="#t"className="alphabet"><button>t</button></a>
-    <a href="#u"className="alphabet"><button>u</button></a>
-    <a href="#v"className="alphabet"><button>v</button></a>
-    <a href="#w"className="alphabet"><button>w</button></a>
-    <a href="#x"className="alphabet"><button>x</button></a>
-    <a href="#y"className="alphabet"><button>y</button></a>
-    <a href="#z"className="alphabet"><button>z</button></a>
-    <a href="##"className="alphabet"><button>#</button></a>
-    
-    <ul id="a">
-        <h2>A</h2>
-    </ul>
-    <ul id="b">
-        <h2>B</h2>
-    </ul>
-    <ul id="c">
-        <h2>C</h2>
-    </ul>
-    <ul id="d">
-        <h2>D</h2>
-    </ul>
-    <ul id="e">
-        <h2>E</h2>
-    </ul>
-    <ul id="f">
-        <h2>F</h2>
-    </ul>
-    <ul id="g">
-        <h2>G</h2>
-    </ul>
-    <ul id="h">
-        <h2>H</h2>
-    </ul>
-    <ul id="i">
-        <h2>I</h2>
-    </ul>
-    <ul id="j">
-        <h2>J</h2>
-    </ul>
-    <ul id="k">
-        <h2>K</h2>
-    </ul>
-    <ul id="l">
-        <h2>L</h2>
-    </ul>
-    <ul id="m">
-        <h2>M</h2>
-    </ul>
-    <ul id="n">
-        <h2>N</h2>
-    </ul>
-    <ul id="o">
-        <h2>O</h2>
-    </ul>
-    <ul id="p">
-        <h2>P</h2>
-    </ul>
-    <ul id="q">
-        <h2>Q</h2>
-    </ul>
-    <ul id="r">
-        <h2>R</h2>
-    </ul>
-    <ul id="s">
-        <h2>S</h2>
-    </ul>
-    <ul id="t">
-        <h2>T</h2>
-    </ul>
-    <ul id="u">
-        <h2>U</h2>
-    </ul>
-    <ul id="v">
-        <h2>V</h2>
-    </ul>
-    <ul id="w">
-        <h2>W</h2>
-    </ul>
-    <ul id="x">
-        <h2>X</h2>
-    </ul>
-    <ul id="y">
-        <h2>Y</h2>
-    </ul>
-    <ul id="z">
-        <h2>Z</h2>
-    </ul>
-    <ul id="#">
-        <h2>#</h2>
-    </ul>
+<nav  classNameName="alphabet_nav">
+    <a classNameName="alphabet"href="#a"><button>a</button></a>
+    <a href="#b"classNameName="alphabet"><button>b</button></a>
+    <a href="#c"classNameName="alphabet"><button>c</button></a>
+    <a href="#d"classNameName="alphabet"><button>d</button></a>
+    <a href="#e"classNameName="alphabet"><button>e</button></a>
+    <a href="#f"classNameName="alphabet"><button>f</button></a>
+    <a href="#g"classNameName="alphabet"><button>g</button></a>
+    <a href="#h"classNameName="alphabet"><button>h</button></a>
+    <a href="#i"classNameName="alphabet"><button>i</button></a>
+    <a href="#j"classNameName="alphabet"><button>j</button></a>
+    <a href="#k"classNameName="alphabet"><button>k</button></a>
+    <a href="#l"classNameName="alphabet"><button>l</button></a>
+    <a href="#m"classNameName="alphabet"><button>m</button></a>
+    <a href="#n"classNameName="alphabet"><button>n</button></a>
+    <a href="#o"classNameName="alphabet"><button>o</button></a>
+    <a href="#p"classNameName="alphabet"><button>p</button></a>
+    <a href="#q"classNameName="alphabet"><button>q</button></a>
+    <a href="#r"classNameName="alphabet"><button>r</button></a>
+    <a href="#s"classNameName="alphabet"><button>s</button></a>
+    <a href="#t"classNameName="alphabet"><button>t</button></a>
+    <a href="#u"classNameName="alphabet"><button>u</button></a>
+    <a href="#v"classNameName="alphabet"><button>v</button></a>
+    <a href="#w"classNameName="alphabet"><button>w</button></a>
+    <a href="#x"classNameName="alphabet"><button>x</button></a>
+    <a href="#y"classNameName="alphabet"><button>y</button></a>
+    <a href="#z"classNameName="alphabet"><button>z</button></a>
+    <a href="##"classNameName="alphabet"><button>#</button></a>
+  </nav>
+<div  className="scroll-container">
+  <div  className="scroll-page" id="a">A</div>
+  <div  className="scroll-page" id="b">B</div>
+  <div  className="scroll-page" id="c">C</div>
+  <div  className="scroll-page" id="d">D</div>
+  <div  className="scroll-page" id="e">E</div>
+  <div  className="scroll-page" id="f">F</div>
+  <div  className="scroll-page" id="g">G</div>
+  <div  className="scroll-page" id="h">H</div>
+  <div  className="scroll-page" id="i">I</div>
+  <div  className="scroll-page" id="j">J</div>
+  <div  className="scroll-page" id="k">K</div>
+  <div  className="scroll-page" id="l">L</div>
+  <div  className="scroll-page" id="m">M</div>
+  <div  className="scroll-page" id="n">N</div>
+  <div  className="scroll-page" id="o">O</div>
+  <div  className="scroll-page" id="p">P</div>
+  <div  className="scroll-page" id="q">Q</div>
+  <div  className="scroll-page" id="r">R</div>
+  <div  className="scroll-page" id="s">S</div>
+  <div  className="scroll-page" id="t">T</div>
+  <div  className="scroll-page" id="u">U</div>
+  <div  className="scroll-page" id="v">V</div>
+  <div  className="scroll-page" id="w">W</div>
+  <div  className="scroll-page" id="x">X</div>
+  <div  className="scroll-page" id="y">Y</div>
+  <div  className="scroll-page" id="z">Z</div>
+  <div  className="scroll-page" id="#">#</div>
+</div>
 
-
-
-    </>);
+  </>);
 const Home = () => {
     return element;
   };
